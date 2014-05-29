61a10bcf477565172ba234dc1d26316e2e0ce296
Evan Chessman <chessman_evan@wheatoncollege.edu>
2014-05-29 14:51:13 -0400

    trying to add showingscd ..cd ..cd ..

--- api/addShowingData.js
+++ api/addShowingData.js
@@ -1,7 +1,7 @@
 var showListings = 		[
 							{title: 'Neighbors', theater: 'Sharon Cinemas 8', hour: [16, 19], minute: [35, 15] },
 							{title: 'Neighbors', theater: 'Showcase Cinema de Lux Patriot Place', hour: [14, 15, 16, 18, 19, 21, 22], minute: [30, 55, 55, 35, 45, 20, 20] },
-							{title: 'Rio 2', theaters: 'Sharon Cinemas 8', hour: [16, 18], minute: [25, 35] },
+							{title: 'Rio 2', theaters: 'Sharon Cinemas 8', hour: [16, 18], minute: [25, 35] }
 							// {title: 'Noah', theater: 'Showcase Cinemas North Attleboro', hour: [22, 22], minute: [15, 15]},
 							// {title: 'Frozen', theater: 'East Providence 10', hour: [12, 14, 17, 19, 21], minute: [40, 50, 5, 20, 30]}
                 		];	
@@ -42,27 +42,43 @@ var Showing = mongoose.model('Showing', ShowingSchema);
 var Dates = []
 
 
-for (var i = 0; i < showListings.length; i++) {
+for (var i = 0; i < showListings.length -1; i++) {
 	for (var x = 0; x < showListings[i].hour.length; x++) {
-		console.log(showListings[i].title);
-		var foo = new Date(2014, 4, 22, showListings[i].hour[x], showListings[i].minute[x] );
-		Dates.push(foo);
-
-		
+		Venue.findOne({name: showListings[i].theater}, "_id", showListings, function (err, venue) {
+			Feature.findOne({title: showListings[i].title}, "_id", showListings, function (err, feature) {		
+				console.log(showListings[i].title);
+				
+				var foo = new Date(2014, 4, 22, showListings[i].hour[x], showListings[i].minute[x]);
+				Dates.push(foo);
+				
+				console.log(venue);
+				console.log(feature);
+				
+				var showtime = new Showing({showtime: Dates[i], venue_id: venue._id, feature_id: feature._id});
+				showtime.save(function(err) {
+					console.log(err);
+				})
+			
+			});
+		});	
 	};
 };
 console.log(Dates);
 
-for (var e = 0; e < showListings.length - 1; e++){
-	Venue.findOne({name: showListings[e].theater}, "_id", function (err, venue) {
-		Feature.findOne({title: showListings[e].title}, "_id", function (err, feature) {		
-			console.log(venue);
-			console.log("after the console log of venue");
-			console.log(feature);
-			console.log("after the console log of feature");
-			Showing.create({showtime: Dates[e], venue_id: venue._id, feature_id: feature._id}, function(err) {});
-		});
-	});
-};
+// for (var e = 0; e < showListings.length; e++){
+// 	Venue.findOne({name: showListings[e].theater}, "_id", function (err, venue) {
+// 		Feature.findOne({title: showListings[e].title}, "_id", function (err, feature) {		
+// 			console.log(venue);
+			
+// 			console.log(feature);
+			
+// 			var showtime = new Showing({showtime: Dates[e], venue_id: venue._id, feature_id: feature._id});
+// 			showtime.save(function(err) {
+// 				console.log(err);
+// 			})
+			
+// 		});
+// 	});
+// };
 
 console.log("Finished the program!");
